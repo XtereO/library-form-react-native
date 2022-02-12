e5a2f0d2c7204f4d4295d7173392fbef590a5b63
XtereO <71135845+XtereO@users.noreply.github.com>
2022-02-12 19:56:43 +0300

    'hooks'

---
+++ hooks/useDoubleClick.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useDoubleClick = (callback: () => void) => {
+  const [clickCount, setClickCount] = useState(0);
+  let timer: number;
+
+  return () => {
+    setClickCount((prev) => prev + 1);
+    if (clickCount === 2) {
+      setClickCount(0);
+      clearTimeout(timer);
+      callback();
+    } else {
+      timer = setTimeout(() => {
+        setClickCount(0);
+      }, 3000);
+    }
+  };
+};
